5887e8d1a211a72de5412dcd8bea540fca4eb107
CCD729 <56373003+CCD729@users.noreply.github.com>
2020-05-10 01:14:06 -0700

    Added audio-reactive colored floor and debug data bars

---
+++ FPS Microgame/Assets/FPS/FFT_Debug_Text.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FFT_Debug_Text : MonoBehaviour
+{
+    public float[] aveMag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Analyze sample data.
+        int numPartitions = 8;
+        aveMag = new float[numPartitions];
+        float partitionIndx = 0;
+        int numDisplayedBins = 512 / 2; //NOTE: we only display half the spectral data because the max displayable frequency is Nyquist (at half the num of bins)
+
+        for (int i = 0; i < numDisplayedBins; i++)
+        {
+            if (i < numDisplayedBins * (partitionIndx + 1) / numPartitions)
+            {
+                aveMag[(int)partitionIndx] += FFT_Handler.spectrumData[i] / (512 / numPartitions);
+            }
+            else
+            {
+                partitionIndx++;
+                i--;
+            }
+        }
+
+        // scale and bound the average magnitude.
+        for (int i = 0; i < numPartitions; i++)
+        {
+            aveMag[i] = (float)0.5 + aveMag[i] * 100;
+            if (aveMag[i] > 100)
+            {
+                aveMag[i] = 100;
+            }
+        }
+
+        for (int i = 1; i <= 8; i++)
+        {
+            if (gameObject.name == "bin" + i)
+            {
+                GetComponent<Text>().text = aveMag[i-1].ToString();
+            }
+        }
+    }
+}
---
+++ FPS Microgame/Assets/FPS/FFT_Display_Bar.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FFT_Display_Bar : MonoBehaviour
+{
+
+    public float[] aveMag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Analyze sample data.
+        int numPartitions = 8;
+        aveMag = new float[numPartitions];
+        float partitionIndx = 0;
+        int numDisplayedBins = 512 / 2; //NOTE: we only display half the spectral data because the max displayable frequency is Nyquist (at half the num of bins)
+
+        for (int i = 0; i < numDisplayedBins; i++)
+        {
+            if (i < numDisplayedBins * (partitionIndx + 1) / numPartitions)
+            {
+                aveMag[(int)partitionIndx] += FFT_Handler.spectrumData[i] / (512 / numPartitions);
+            }
+            else
+            {
+                partitionIndx++;
+                i--;
+            }
+        }
+
+        // scale and bound the average magnitude.
+        for (int i = 0; i < numPartitions; i++)
+        {
+            aveMag[i] = (float)0.5 + aveMag[i] * 100;
+            if (aveMag[i] > 100)
+            {
+                aveMag[i] = 100;
+            }
+        }
+
+        for (int i = 1; i <= 8; i++)
+        {
+            if (gameObject.name == "FFTDisplayBar"+i)
+            {
+                GetComponent<RectTransform>().localScale = new Vector3(GetComponent<RectTransform>().localScale.x, aveMag[i-1], GetComponent<RectTransform>().localScale.z);
+            }
+        }
+    }
+}
